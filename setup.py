715929fcb067a3205cb9ea7d648d382b71a870bd
Sean Robertson <sdrobert@cs.toronto.edu>
2023-02-21 15:37:27 -0500

    patch

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@
 
 import numpy
 import setuptools
+from Cython.Build import cythonize
 
-setuptools.setup(
-    ext_modules=[setuptools.Extension(
+extensions = [setuptools.Extension(
         'libri_light_dtw',
         sources=['libriabx/libri_light/ABX_src/dtw.pyx'],
         extra_compile_args=['-O3'],
-        include_dirs=[numpy.get_include()])],
+        include_dirs=[numpy.get_include()])]
 
+setuptools.setup(
+    ext_modules=cythonize(extensions, force=True),
     # needed for cython/setuptools, see
     # http://docs.cython.org/en/latest/src/quickstart/build.html
     zip_safe=False
